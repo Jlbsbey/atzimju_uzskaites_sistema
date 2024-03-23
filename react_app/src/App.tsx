04685aa3c6f1ee31ccdb26f90f7e41c9f180c0f5
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-03-23 16:49:23 +0200

    Unnecessary removed

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/App.css';
+import "bootstrap/dist/js/bootstrap.bundle.js.map";
+import "bootstrap/dist/css/bootstrap.min.css";
+import LoginComponent from "./components/LoginComponent";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+
+			<LoginComponent/>
+
+		</div>
+	);
 }
 
 export default App;
