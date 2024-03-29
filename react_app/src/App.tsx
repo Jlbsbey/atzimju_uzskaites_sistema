034bc8eb36ffb71159c2920b40f0414afd61233e
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-03-29 21:04:23 +0200

    About page and some imports added.

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import "./styles/global.css";
+import "./styles/typography.css";
+import "./styles/layout_global.css";
 import "bootstrap/dist/js/bootstrap.bundle.js.map";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "./components/Layout";
 import LoginPage from "./pages/LoginPage";
 import MainPage from "./pages/MainPage";
 import The404Page from "./pages/The404Page";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
 	return (
@@ -15,6 +18,7 @@ function App() {
 				<Route path="/" element={<Layout />}>
 					<Route index element={<LoginPage />} />
 					<Route path="main" element={<MainPage />} />
+					<Route path="about" element={<AboutPage />} />
 					<Route path="*" element={<The404Page />} />
 				</Route>
 			</Routes>
