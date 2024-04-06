5ed7cefee264e4b8690c6a5bada54d5fa84b6c98
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-04-06 20:18:27 +0300

    Bug fix?

--- react_app/src/scripts/login.ts
+++ react_app/src/scripts/login.ts
@@ -1,38 +1,15 @@
 import {setAuthCookie} from "./cookies";
-
-interface Response {
-	login_status: boolean;
-	session_key: string;
-	expire_time: string;
-}
+import {sendRequest} from "./request";
 
 export function user_login(username: string, password: string): Promise<boolean> {
-	let response = try_login("https://grade.nevolodia.com:8443/login", username, password);
+	let response = sendRequest("login", "username=" + username + "&password=" + password);
 
-	return response.then((data: Response) => {
+	return response.then((data: any) => {
+		console.log(data);
 		if (data.login_status) {
 			setAuthCookie(data.session_key, data.expire_time);
 			return true;
 		}
 		return false;
 	});
-}
-
-function try_login(url: string, username: string, password: string): Promise<Response> {
-	let urlRequest = url + "?username=" + username + "&password=" + password;
-	// return as Response
-	return fetch(urlRequest)
-		.then(response => {
-			if (!response.ok) {
-				throw new Error('Network response was not ok');
-			}
-			return response.json();
-		})
-		.then(jsonData => {
-			return jsonData;
-		})
-		.catch(error => {
-			console.error('Error fetching data:', error.message);
-			throw error;
-		});
 }
