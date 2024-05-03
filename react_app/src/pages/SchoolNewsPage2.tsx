e743b1aa2e454d7a72aa4154ad69c392bd6c4aca
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-05-03 13:26:43 +0300

    Bug fixed. Tests added.

---
+++ react_app/src/pages/SchoolNewsPage2.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "../styles/user_page.css";
+import 'simplebar-react/dist/simplebar.min.css';
+import logo from "../assets/images/kabul.png";
+import PeVideo from "../components/PeVideo";
+
+const SchoolNewsPage: React.FC = () => {
+
+	return (
+		<div className="">
+			<h1 className="">
+				[Archive] Kabul International - 26.12.1979
+			</h1>
+			<p style={{fontSize: "20px", fontWeight: "500"}}>
+				The breaking news about soviet invasion of Afghanistan. <br/>
+				Learn how Afghanistan is living, fighting, and suffering <br/>
+				on the 3rd day of the invasion.
+			</p>
+			<img src={logo}
+				 style={{width: "35%", marginBottom: "30px"}}
+			/>
+
+			<div style={{width: "85%"}}>
+				<PeVideo src={"https://negallery.ams3.cdn.digitaloceanspaces.com/temp/final.mp4"}
+						 provider={"html5"}
+				/>
+			</div>
+
+		</div>
+	);
+}
+
+export default SchoolNewsPage;
