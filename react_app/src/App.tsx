a535c02ff18e65d75d226c87c743b93014fe6205
hackername1 <62359997+hackername1@users.noreply.github.com>
2024-05-03 13:24:11 +0300

    Bug fixed. Tests added.

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -13,6 +13,8 @@ import AboutPage from "./pages/AboutPage";
 import SessionEndedPage from "./pages/SessionEndedPage";
 import UserPage from "./pages/UserPage";
 import AdminHomePage from "./pages/AdminHomePage";
+import SchoolNewsPage from "./pages/SchoolNewsPage";
+import SchoolNewsPage2 from "./pages/SchoolNewsPage2";
 
 function App() {
 	return (
@@ -25,6 +27,9 @@ function App() {
 					<Route path="about" element={<AboutPage />} />
 					<Route path="session_ended" element={<SessionEndedPage />} />
 					<Route path="admin" element={<AdminHomePage />} />
+
+					<Route path="kabul" element={<SchoolNewsPage />} />
+					<Route path="kabul-lq" element={<SchoolNewsPage2 />} />
 					<Route path="*" element={<The404Page />} />
 				</Route>
 			</Routes>
